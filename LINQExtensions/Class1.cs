229f627e4ae44a82a15d73ca4322b07b9a0f9398
Anastasia Soboleva <flamrosae@yandex.ru>
2022-07-27 16:13:14 +0500

    Enshure

---
+++ LINQExtensions/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+namespace LINQExtensions;
+public static class LinqExtensions 
+{
+    public static bool Ensure<T>(this IEnumerable<T> enumerable, Predicate<T> method)
+    {
+        foreach (T item in enumerable)
+            if (!method(item)) throw new NotImplementedException();
+        return true;
+    }
+}
