ec592854f789839a7345d080afa209e0d692f88a
Anastasia Soboleva <flamrosae@yandex.ru>
2022-07-31 14:43:34 +0500

    LINQ

--- LINQExtensions/Class1.cs
+++ LINQExtensions/Class1.cs
@@ -1,11 +1,37 @@
 ﻿using System.Linq;
 namespace LINQExtensions;
-public static class LinqExtensions 
+public static class IEnumerableExtensions
 {
-    public static bool Ensure<T>(this IEnumerable<T> enumerable, Predicate<T> method)
+    public static IEnumerable<T> Ensure<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate)
     {
         foreach (T item in enumerable)
-            if (!method(item)) throw new NotImplementedException();
-        return true;
+        {
+            if (!predicate(item)) throw new ArgumentException(); 
+            else yield return item;
+        }        
     }
+    public static IEnumerable<T> EnshureCount<T>(this IEnumerable<T> enumerable, Func<int, bool> predicate)
+    {
+        var count = 0;
+        foreach (T item in enumerable)
+        {   
+            count++;
+            yield return item;
+        }
+        if (!predicate(count)) throw new ArgumentException();
+    }
+    public static bool Any<T>(this IEnumerable<T> enumerable, int count, Func<T, Boolean> predicate)
+    {
+        var index = 0;
+        foreach(T item in enumerable)
+            if(predicate(item)) index++;
+        return index == count;
+    }
+    public static bool IsEmpty<T>(this IEnumerable<T> enumerable) => (enumerable.Count() == 0 || enumerable == null) ? true : false;
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> enumerable, int length)
+    {
+        var values = new List<IEnumerable<T>>();
+
+    }
+
 }
